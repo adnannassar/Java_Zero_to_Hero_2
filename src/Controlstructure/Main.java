3dd599ebe4784e89faf2862b7f0df5ced3651a5e
adnan <adnan.nassar90@gmail.com>
2023-04-01 19:15:47 +0200

    added: if in for example

---
+++ src/Controlstructure/Main.java
@@ -0,0 +1,15 @@
+package Controlstructure;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 10; i >= -5; i--) {
+            int nr1 = i;
+            if (nr1 != 0) {
+                int result = 100 / nr1;
+                System.out.println(100 + " / (i) " + nr1 + " = " + result);
+            }else{
+                System.out.println(100 + " / (i) " + nr1 + " = " + "Undefined");
+            }
+        }
+    }
+}
